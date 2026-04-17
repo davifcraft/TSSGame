55c364588f11729bc44735b3d4c9063ae5bcab38
davifcraft <freiredavi665@gmail.com>
2026-04-17 17:06:16 -0300

    adding the filesss

---
+++ TSSGame/src/TSSGameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TSSGameFrame extends JFrame {
+
+    public TSSGameFrame() {
+        setBackground(Color.BLACK);
+        setTitle("The Shadow System -- The Converter Crisis");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
